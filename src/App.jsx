7b1fd9bd3dd577b7b79b7811d2a3add7a52ef1d3
Teo-Fer <ismafer0007@gmail.com>
2025-04-24 22:52:56 -0300

    feat: :zap: [ComingSoon] Se implementa fondo con particulas

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,25 @@
 import './App.css'
 import ComingSoon from './pages/ComingSoon'
-
 import '@fontsource-variable/jost';
-import Background from './components/Background'
-
-
+import ParticlesBg from 'particles-bg'
 
 
 function App() {
-  const targetDate = new Date('2025-04-28T09:00:00'); // Cambia esta fecha a la que desees  
+  const targetDate = new Date('2025-04-28T09:00:00'); // Cambia esta fecha a la que desees
   return (
      <div style={styles.app}>
-      {/* <Background/> */}
-      <ComingSoon targetDate={targetDate}/>  
-       {/* <Footer /> */}
+      <ComingSoon targetDate={targetDate}/>
+      <ParticlesBg color="#0070be" num={100} type="cobweb" bg={true} />
      </div>
-   );
+   ); 
 }
 
 const styles = {
   app: {
     display: 'flex',
     flexDirection: 'column',
-    minHeight: '100vh',
+    alignItems: 'center',
+    padding: '1rem',
   },
 };
 
--- src/pages/ComingSoon.jsx
+++ src/pages/ComingSoon.jsx
@@ -1,30 +1,21 @@
 import React, { useState } from 'react';
 import './ComingSoon.css';
 import FlipClockCountdown from '@leenguyen/react-flip-clock-countdown';
-import { LightModeOutlined } from '@mui/icons-material';
-import { DarkModeOutlined } from '@mui/icons-material';
-
 import '@leenguyen/react-flip-clock-countdown/dist/index.css';
 import { TypeAnimation } from 'react-type-animation';
 import degiLogo from '../assets/logo.png'
 
 const ComingSoon = ({ targetDate }) => {
-
-    const [isDarkTheme, setIsDarkTheme] = useState(false);
-
-    const toggleTheme = () => {
-        setIsDarkTheme(!isDarkTheme);
-    };
-
     return (
-        <div className={`coming-soon-container ${isDarkTheme ? 'dark' : 'light'}`}>
+        <div className={`coming-soon-container`}>
+            {/* <Background style={{color:"red"}}/> */}
             <img src={degiLogo} alt="Logo" className="logo" />
             <TypeAnimation
                 sequence={[
                     // Same substring at the start will only be typed out once, initially
-                    'Se viene cositas...',
+                    'Se vienen cositas...',
                     1000,
-                    'Se viene cositas...',
+                    'Se vienen cositas...',
                     1000
                 ]}
                 speed={50}
@@ -34,19 +25,10 @@ const ComingSoon = ({ targetDate }) => {
             <FlipClockCountdown
                 to={targetDate}
                 labels={['Días', 'Horas', 'Minutos', 'Segundos']}
-                labelStyle={{ fontSize: '1rem', fontWeight: 500, textTransform: 'uppercase' }}
-                digitBlockStyle={{ width: '2em', height: '3em', fontSize: '2rem' }} // Cambia a un porcentaje
-                dividerStyle={{ color: 'gray', height: 1 }}
-                separatorStyle={{ color: 'white', size: '0.5em' }}
                 duration={0.5}
                 className='flip-clock'
-                style={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap', padding: '1em' }} // Permitir que se envuelvan
-            >
+                >
             </FlipClockCountdown>
-
-            <button onClick={toggleTheme} className="theme-toggle">
-                {isDarkTheme ? <LightModeOutlined /> : <DarkModeOutlined />}
-            </button>
         </div>
     );
 };
